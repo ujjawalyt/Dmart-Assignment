1b1a5194ebb26e31bc99a703c4a3cf691aad39e4
= <=>
2023-07-15 07:32:07 +0530

    project structure done

---
+++ StockManagementSystem/src/main/java/com/stock/StockManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementSystemApplication.class, args);
+	  System.out.println("Stock Management System Application is Start Running...");
+	}
+
+}
